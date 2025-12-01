79d5af29c6b8d54b4ba9eece18a529dbe90185e9
mariana <165570206+marianafranca0@users.noreply.github.com>
2025-12-01 14:05:18 -0300

    Incrementa controller com put e delete

--- src/main/java/br/com/proiecte/service/ProjetoService.java
+++ src/main/java/br/com/proiecte/service/ProjetoService.java
@@ -1,12 +1,49 @@
 package br.com.proiecte.service;
 
+import br.com.proiecte.model.Projeto;
 import br.com.proiecte.repository.ProjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProjetoService {
 
     @Autowired
     public ProjetoRepository projetoRepository;
 
+    // método save()
+    public Projeto save(Projeto projeto){
+        return projetoRepository.save(projeto);
+
+    }
+
+    // método findAll()
+    public List<Projeto> findAll(){
+        return projetoRepository.findAll();
+  }
+
+    // método update()
+    public void update(Projeto projeto){
+        // verifica se o projeto existe antes de atualizar
+        Optional<Projeto> existe = projetoRepository.findById(projeto.getId());
+        if(existe.isPresent()) {
+            projetoRepository.save(projeto); // atualiza
+        } else{
+            throw new RuntimeException("projeto não encontrado.");
+        }
+    }
+
+    // método delete()
+    public void delete(Long id) {
+        if (projetoRepository.existsById(id)) {
+            projetoRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("projeto não encontrado.");
+        }
+    }
 
 }
+
